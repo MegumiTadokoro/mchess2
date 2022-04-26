f978afa862ee6574ae7aecdb7755990593affa1d
Nguyễn Doãn Đại <40011497+MegumiTadokoro@users.noreply.github.com>
2022-04-26 17:19:20 +0200

    Parallelize

--- main.cc
+++ main.cc
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <chrono>
 #include <random>
+#include <algorithm>
 
 #include "CBoard.h"
 #include "CPerft.h"
@@ -11,88 +12,98 @@
 #include "CSearchSuite.h"
 #include "ai.h"
 #include "trace.h"
+#include "parallel_for.h"
 
 #ifdef ENABLE_TRACE
 std::ostream *gpTrace = 0;
 #endif
 
+#define PARALLEL_FOR_BEGIN(nb_elements) parallel_for(nb_elements, [&](int start, int end){ for(int i = start; i < end; ++i)
+#define PARALLEL_FOR_END()})
+
 // Experiment script
 
+const int n = 6;
 
+void match(
+    int player,
+    double strength, 
+    bool isPlayingWhite, 
+    int (&arr)[n][3],
+    bool againsStockFish)
+{
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator (seed);
+    std::uniform_real_distribution<double> distribution(0.0,1.0);
+    CBoard board;
+    AI gm = AI(board), levy = AI(board);
+    while (true)
+    {
+        CMove best_move;
+        if (isPlayingWhite && board.whiteToMove())
+        {
+            best_move = levy.find_best_or_worst_move(distribution(generator) <= strength);
+        }
+        else
+        {
+            best_move = gm.find_best_or_worst_move(againsStockFish);
+        }
+        if (!best_move.Valid())
+        {
+            // Oops. No legal move was found
+            bool check = board.isOtherKingInCheck();
+            if (check) arr[player][board.whiteToMove() == isPlayingWhite ? 0 : 2]++;
+            else arr[player][1]++;
+            break;
+        }
+        board.make_move(best_move);
+
+        // 50-move rule
+        if(board.fiftyMoveDraw())
+        {
+            //std::cerr << "Evoke draw by 50-move rule\n";
+            arr[player][1]++;
+            break;
+        }
+    }
+}
 
 int main()
 {
     freopen("result.csv", "w", stdout);
-    int n = 11, n_games = 1, StinkFish[n][3], StockFish[n][3];
+    int n_games = 20, StinkFish[n][3], StockFish[n][3];
+    memset(StinkFish, 0, sizeof(StinkFish));
+    memset(StockFish, 0, sizeof(StockFish));
     
-    unsigned seed = 2022;//std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine generator (seed);
-    std::uniform_real_distribution<double> distribution(0.0,1.0);
-    for (int i = 0; i < n; i++)
+    pl::async_par_for(0, n, [&](int i)
     {
-        double strength = (double) i/(n-1);
-        bool isPlayingWhite = true;
-        for (int j = 0; j < n_games; j++)
+        pl::async_par_for(0, n_games, [&](int j)
         {
             std::cerr << "Against StinkFish " << i << ' ' << j << '\n';
-            CBoard board = CBoard();
-            AI gm = AI(board), levy = AI(board);
-            while (true)
-            {
-                CMove best_move;
-                if (isPlayingWhite && board.whiteToMove())
-                {
-                    best_move = levy.find_best_or_worst_move(distribution(generator) <= strength);
-                }
-                else
-                {
-                    best_move = gm.find_best_or_worst_move(false);
-                }
-                if (!best_move.Valid())
-                {
-                    // Oops. No legal move was found
-                    bool check = board.isOtherKingInCheck();
-                    if (check) StinkFish[i][board.whiteToMove() == isPlayingWhite ? 0 : 2]++;
-                    else StinkFish[i][1]++;
-                    break;
-                }
-                board.make_move(best_move);
-            }
+            match(i, (double) i/(n-1), j&1, StinkFish, false);
+        });
 
-            isPlayingWhite = !isPlayingWhite;
-        }
-
-        isPlayingWhite = true;
-        for (int j = 0; j < n_games; j++)
+        pl::async_par_for(0, n_games, [&](int j)
         {
             std::cerr << "Against StockFish " << i << ' ' << j << '\n';
-            CBoard board = CBoard();
-            AI gm = AI(board), levy = AI(board);
-            while (true)
-            {
-                CMove best_move;
-                if (isPlayingWhite && board.whiteToMove())
-                {
-                    best_move = levy.find_best_or_worst_move(distribution(generator) <= strength);
-                }
-                else
-                {
-                    best_move = gm.find_best_or_worst_move(true);
-                }
-                if (!best_move.Valid())
-                {
-                    // Oops. No legal move was found
-                    bool check = board.isOtherKingInCheck();
-                    if (check) StockFish[i][board.whiteToMove() == isPlayingWhite ? 0 : 2]++;
-                    else StockFish[i][1]++;
-                    break;
-                }
-                board.make_move(best_move);
-            }
+            match(i, (double) i/(n-1), j&1, StockFish, true);
+        });
+    });
 
-            isPlayingWhite = !isPlayingWhite;
-        }
-    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     double strength = (double) i/(n-1);
+    //     for (int j = 0; j < n_games; j++)
+    //     {
+    //         std::cerr << "Against StinkFish " << i << ' ' << j << '\n';
+    //         match(i, strength, j&1, StinkFish, false, seed);
+    //     }
+    //     for (int j = 0; j < n_games; j++)
+    //     {
+    //         std::cerr << "Against StockFish " << i << ' ' << j << '\n';
+    //         match(i, strength, j&1, StockFish, true, seed);
+    //     }
+    // }
 
     for (int i = 0; i < n; i++)
     {
